0fca1deb8c53142c10833da632a769520e83f3fe
neitoo <samuraa855@gmail.com>
2022-11-05 20:52:02 +0500

    registration implemented (verified) login implemented (verified)(not save user)

--- app/src/main/java/ru/neito/habitlyrpg/LoginActivity.kt
+++ app/src/main/java/ru/neito/habitlyrpg/LoginActivity.kt
@@ -1,5 +1,6 @@
 package ru.neito.habitlyrpg
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,56 +8,62 @@ import android.text.TextUtils
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
-        registerBTR.setOnClickListener {
+        registerBTL.setOnClickListener{
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginBTL.setOnClickListener {
             when{
-                TextUtils.isEmpty(emailETR.text.toString().trim{ it <= ' '}) -> {
+                TextUtils.isEmpty(emailETL.text.toString().trim{ it <= ' '}) -> {
                     Toast.makeText(
-                        this@RegisterActivity,
+                        this@LoginActivity,
                         "Пожалуйста введите email.",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
 
-                TextUtils.isEmpty(passwordETR.text.toString().trim{ it <= ' '}) -> {
+                TextUtils.isEmpty(passwordETL.text.toString().trim{ it <= ' '}) -> {
                     Toast.makeText(
-                        this@RegisterActivity,
+                        this@LoginActivity,
                         "Пожалуйста введите пароль.",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
                 else ->{
-                    val email: String = emailETR.text.toString().trim{ it <= ' '}
-                    val password: String =passwordETR.text.toString().trim {it <= ' '}
+                    val email: String = emailETL.text.toString().trim{ it <= ' '}
+                    val password: String =passwordETL.text.toString().trim {it <= ' '}
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
                                 val firebaseUser: FirebaseUser = task.result!!.user!!
 
                                 Toast.makeText(
-                                    this@RegisterActivity,
-                                    "Успешная регистрация в мире HabitlyRPG.",
+                                    this@LoginActivity,
+                                    "Добро пожаловать в HabitlyRPG.",
                                     Toast.LENGTH_SHORT
                                 ).show()
 
                                 val intent =
-                                    Intent(this@RegisterActivity, MainMenuActivity::class.java)
+                                    Intent(this@LoginActivity, MainMenuActivity::class.java)
                                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                intent.putExtra("user_id", firebaseUser.uid)
+                                intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
                                 intent.putExtra("email_id", email)
                                 startActivity(intent)
                                 finish()
                             }
                             else{
                                 Toast.makeText(
-                                    this@RegisterActivity,
+                                    this@LoginActivity,
                                     task.exception!!.message.toString(),
                                     Toast.LENGTH_SHORT
                                 ).show()
@@ -64,11 +71,6 @@ class RegisterActivity : AppCompatActivity() {
                         }
                 }
             }
-        }
-
-        backBT.setOnClickListener {
-            onBackPressed()
-
         }
     }
 }
