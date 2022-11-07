b15f7e593a2d21cc3e29fe8a9f1e0de0323c8510
neitoo <samuraa855@gmail.com>
2022-11-07 19:50:06 +0500

    implemented autologin/logout

--- app/src/main/java/ru/neito/habitlyrpg/LoginActivity.kt
+++ app/src/main/java/ru/neito/habitlyrpg/LoginActivity.kt
@@ -2,19 +2,52 @@ package ru.neito.habitlyrpg
 
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.CompoundButton
 import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var sharePref: SharedPreferences
+    var isRemembered = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        sharePref = getSharedPreferences("SHARED_PREF",MODE_PRIVATE)
+
+        val emailShare = sharePref.getString("emailID","")
+        val passwordShare = sharePref.getString("password","")
+        val checkboxShare = sharePref.getBoolean("rememberMe", false)
+
+        rememberMeCheck.isChecked = checkboxShare
+
+        if (rememberMeCheck.isChecked){
+            emailETL.setText(emailShare)
+            passwordETL.setText(passwordShare)
+
+            val email: String = emailETL.text.toString().trim{ it <= ' '}
+            val password: String =passwordETL.text.toString().trim {it <= ' '}
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    userLogin(task, email)
+                }
+
+
+
+        }
+
+
+
 
         registerBTL.setOnClickListener{
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
@@ -39,38 +72,58 @@ class LoginActivity : AppCompatActivity() {
                     ).show()
                 }
                 else ->{
+                    val editor = sharePref.edit()
                     val email: String = emailETL.text.toString().trim{ it <= ' '}
                     val password: String =passwordETL.text.toString().trim {it <= ' '}
 
+                    if (rememberMeCheck.isChecked){
+                        editor.putString("emailID", email)
+                        editor.putString("password", password)
+                        editor.putBoolean("rememberMe", true)
+                        editor.apply()
+                    }
+                    else if(!rememberMeCheck.isChecked){
+                        editor.putString("emailID", "")
+                        editor.putString("password", "")
+                        editor.putBoolean("rememberMe", false)
+                        editor.apply()
+                    }
+
                     FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
                         .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                val firebaseUser: FirebaseUser = task.result!!.user!!
-
-                                Toast.makeText(
-                                    this@LoginActivity,
-                                    "Добро пожаловать в HabitlyRPG.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-
-                                val intent =
-                                    Intent(this@LoginActivity, MainMenuActivity::class.java)
-                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
-                                intent.putExtra("email_id", email)
-                                startActivity(intent)
-                                finish()
-                            }
-                            else{
-                                Toast.makeText(
-                                    this@LoginActivity,
-                                    task.exception!!.message.toString(),
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
+                            userLogin(task, email)
                         }
                 }
             }
+        }
+    }
+
+    private fun userLogin(
+        task: Task<AuthResult>,
+        email: String
+    ) {
+        if (task.isSuccessful) {
+            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+            Toast.makeText(
+                this@LoginActivity,
+                "Добро пожаловать в HabitlyRPG.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            val intent =
+                Intent(this@LoginActivity, MainMenuActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+            intent.putExtra("email_id", email)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(
+                this@LoginActivity,
+                task.exception!!.message.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
--- app/src/main/java/ru/neito/habitlyrpg/MainMenuActivity.kt
+++ app/src/main/java/ru/neito/habitlyrpg/MainMenuActivity.kt
@@ -1,11 +1,36 @@
 package ru.neito.habitlyrpg
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main_menu.*
+import java.util.prefs.AbstractPreferences
 
 class MainMenuActivity : AppCompatActivity() {
+    lateinit var sharePref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        sharePref = getSharedPreferences("SHARED_PREF",MODE_PRIVATE)
+
+        val userId = intent.getStringExtra("user_id")
+        val emailId = intent.getStringExtra("email_id")
+
+        uidText.text = "User ID :: $userId"
+        mailText.text = "Email ID :: $emailId"
+
+        logoutBT.setOnClickListener {
+            val editor = sharePref.edit()
+            editor.clear()
+            editor.apply()
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this@MainMenuActivity, LoginActivity::class.java))
+            finish()
+        }
     }
 }
